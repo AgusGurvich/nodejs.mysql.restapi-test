06bf6848442a8d6db05b5c3dca69ed3cf3ec8233
Agus Gurvich <agus.gurvich@gmail.com>
2023-04-18 10:26:05 -0300

    parche de errores ping

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,6 +2,13 @@ import { pool } from "../db.js";
 
 
 export const pong = async (req, res)=>{
+    try {
+     
     const [result] = await pool.query('SELECT "pong" AS result');
-       res.json(result);
+    res.json(result);   
+    } catch (error) {
+        res.status(500).json({
+            message: "lo siento bebeeee"
+        })
+    }
    }
